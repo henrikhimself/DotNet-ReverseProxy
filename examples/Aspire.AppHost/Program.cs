2da5497b688f7d8c33d9aec89bbbdc38b6169006
Henrik Jensen <fc1ee26f@gmail.com>
2025-10-08 17:25:00 +0200

    Improve reverse proxy example port config.

--- examples/Aspire.AppHost/Program.cs
+++ examples/Aspire.AppHost/Program.cs
@@ -5,17 +5,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 // Add example website.
 var website = builder.AddProject<Examples_Aspire_Website>("Website", options =>
-{
-  // Do not use endpoint configuration found in the Website project. We let aspire set up everything.
-  options.ExcludeLaunchProfile = true;
-  options.ExcludeKestrelEndpoints = true;
-})
+  {
+    // Do not use endpoint configuration found in the Website project. We let aspire set up everything.
+    options.ExcludeLaunchProfile = true;
+    options.ExcludeKestrelEndpoints = true;
+  })
   // Add a HTTP endpoint. The reverse proxy will set up a secure HTTPS endpoint for you to connect to this resource.
   .WithHttpEndpoint();
 
 // Add reverse proxy website.
 var reverseProxy = builder
   .AddProject<Examples_Aspire_ReverseProxy>("Reverse-Proxy")
+  // Configure reverse proxy to use https on port 443.
+  .WithHttpsEndpoint(443)
   // Map a host name to the endpoint of the example website.
   .WithReverseProxyReference("Website", website.GetEndpoint("http"), "example-website.local");
 
