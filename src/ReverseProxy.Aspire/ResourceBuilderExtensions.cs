b0cf0b1bd8b071e8a330e662e45bc261f8534046
Henrik Jensen <fc1ee26f@gmail.com>
2026-01-19 07:39:33 +0100

    Fix Aspire extension package references

--- src/ReverseProxy.Aspire/ResourceBuilderExtensions.cs
+++ src/ReverseProxy.Aspire/ResourceBuilderExtensions.cs
@@ -14,6 +14,9 @@
 // limitations under the License.
 // </copyright>
 
+using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+
 namespace Hj.ReverseProxy.Aspire;
 
 public static class ResourceBuilderExtensions
